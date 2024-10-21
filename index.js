472f102ee272e68e9c57e4beb911caf6e12c5cbf
JEREMY TIPTON <32341894+tiptonspiderj@users.noreply.github.com>
2024-10-21 11:52:11 -0400

    updated app

--- index.js
+++ index.js
@@ -5,33 +5,13 @@ let homeScore = 0
 let awayScore = 0
 
 
-function homeadd1() {
-    homeScore += 1
+function homeadd(increment) {
+    homeScore += increment
     home.textContent = homeScore
 }
 
-function homeadd2() {
-    homeScore += 2
-    home.textContent = homeScore
-}
-
-function homeadd3() {
-    homeScore += 3
-    home.textContent = homeScore
-}
-
-function awayadd1() {
-    awayScore += 1
-    away.textContent = awayScore
-}
-
-function awayadd2() {
-    awayScore += 2
-    away.textContent = awayScore
-}
-
-function awayadd3() {
-    awayScore += 3
+function awayadd(increment) {
+    awayScore += increment
     away.textContent = awayScore
 }
 
